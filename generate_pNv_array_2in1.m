5e0bfe6669918471ed1dae996ad760dc56021727
bishopAL <470797339@qq.com>
2019-03-11 13:32:32 +0800

    Update generate_pNv_array_2in1.m
    optimize the m file again

--- generate_pNv_array_2in1.m
+++ generate_pNv_array_2in1.m
@@ -1,35 +1,34 @@
-syms p_alpha p_beta p_gamma px py pz t % If you are seeking for the details of the equation
-%Plz go for the paper written by Yang Bin.
-%Parameter Setting
+syms p_alpha p_beta p_gamma px py pz t
+% If you are seeking for the details of the equation
+% Plz go for the paper written by Yang Bin.
+
+% Parameter Setting
 L1 = 65;
 L2 = 65;
 L3 = 10;
-DATANUM = 80;
-TIME_PERIORD = [0 2];
-TIME_PER_STEP = (TIME_PERIORD(2) - TIME_PERIORD(1)) / DATANUM;
+DATA_NUM = 80.0;
+TIME_PERIOD = [0.0, 2.0];
+TIME_PER_STEP = (TIME_PERIOD(2) - TIME_PERIOD(1)) / DATA_NUM;
 
 syms p_alpha p_beta p_gamma px py pz t
-%Gait planning
+% Gait planning
+p_array = [];
+v_array = [];
 px = L1;
 py = L2 + 10 * t;
 pz = -L3 - 10 * t^2 + 40 * t;
-p_array = [];
-v_array = [];
 % px = 65;
 % py = 85;
 % pz = 30 - 80 * (t-2);
 
-for i=TIME_PERIORD(1):TIME_PER_STEP:TIME_PERIORD(2)
+for i=TIME_PERIOD(1):TIME_PER_STEP:TIME_PERIOD(2)
     disp(i);   
     p_alpha = asin(-L3/(px^2+pz^2)^0.5)-atan2(pz,px);
     v_alpha = diff(p_alpha,t);
     p_beta = asin((L1^2 + L2^2 + L3^2 - px^2 - py^2 - pz^2)/(2*L1*L2));
     v_beta = diff(p_beta,t);
-    p_gamma = asin((px^2+py^2+pz^2+L1^2-L2^2-L3^2)/(2*L1*(px^2+py^2+pz^2-L3^2)^0.5))-atan2((px^2+pz^2-L3^2)^0.5,py);
+    p_gamma = asin((px^2+py^2+pz^2+L1^2-L2^2-L3^2) / (2*L1*(px^2+py^2+pz^2-L3^2)^0.5)) - atan2((px^2+pz^2-L3^2)^0.5,py);
     v_gamma = diff(p_gamma,t);
-%     px_para = px;
-%     py_para = double(subs(py,t,i));
-%     pz_para = double(subs(pz,t,i));
     p_array = [p_array 
                [double(subs(p_alpha, t, i)), double(subs(p_beta, t, i)), double(subs(p_gamma, t, i))]];
     v_array = [v_array 
