f06069314e40949e19bfc723bddb4f5aefd3950a
Bingcheng Wang <470797339@qq.com>
2021-08-10 10:19:18 +0800

    add some parameter

--- dynamixel_cpp_controller/src/dynamixel.cpp
+++ dynamixel_cpp_controller/src/dynamixel.cpp
@@ -1,4 +1,5 @@
 #include "dynamixel.h"
+
 using namespace std;
 DxlAPI::DxlAPI()
 {
@@ -14,23 +15,44 @@ DxlAPI::setID(int id[])
     {
         ID.push_back(id[i]);
     }
-    cout << "The ID has been set to " << ID << " ." << endl;
+    std::cout << "The ID has been set to " << ID << " ." << std::endl;
 }
 
 DxlAPI::setPort(string port)
 {
     DEVICENAME = port;
-    cout << "The port has been set to " << DEVICENAME << " ." << endl;
+    std::cout << "The port has been set to " << DEVICENAME << " ." << std::endl;
 }
 
 DxlAPI::setBaudRate(int baudRate)
 {
     BAUDRATE = baudRate;
-    cout << "The baudRate has been set to " << BAUDRATE << " ." << endl;
+    std::cout << "The baudRate has been set to " << BAUDRATE << " ." << std::endl;
 }
 
 DxlAPI::connectToMotors()
 {
     dynamixel::PortHandler *portHandler = dynamixel::PortHandler::getPortHandler(DEVICENAME);
     dynamixel::PacketHandler *packetHandler = dynamixel::PacketHandler::getPacketHandler(2.0);
+    // Initialize GroupSyncWrite
+    dynamixel::GroupSyncWrite groupSyncWriteCurrent(portHandler, packetHandler, ADDR_PRO_GOAL_CURRENT, ADDR_PRO_GOAL_CURRENT_LENGTH);
+    dynamixel::GroupSyncWrite groupSyncWritePosition(portHandler, packetHandler, ADDR_PRO_GOAL_POSITION, ADDR_PRO_GOAL_POSITION_LENGTH);
+    dynamixel::GroupSyncWrite groupSyncWriteVelocity(portHandler, packetHandler, ADDR_PRO_GOAL_VELOCITY, ADDR_PRO_GOAL_VELOCITY_LENGTH);
+    dynamixel::GroupSyncWrite groupSyncWriteProfileVelocity(portHandler, packetHandler, ADDR_PRO_PROFILE_VELOCITY, ADDR_PRO_PROFILE_VELOCITY_LENGTH);
+    dynamixel::GroupSyncWrite groupSyncWriteProfileAcceleration(portHandler, packetHandler, ADDR_PRO_PROFILE_ACCELERATION, ADDR_PRO_PROFILE_ACCELERATION_LENGTH);
+    dynamixel::GroupSyncWrite groupSyncWriteTorqueEnable(portHandler, packetHandler, ADDR_PRO_TORQUE_ENABLE, ADDR_PRO_TORQUE_ENABLE_LENGTH);
+    // Initialize GroupSyncRead
+    dynamixel::GroupSyncRead groupSyncReadPosition(portHandler, packetHandler, ADDR_PRO_PRESENT_POSITION, ADDR_PRO_PRESENT_POSITION_LENGTH);
+    dynamixel::GroupSyncRead groupSyncReadVelocity(portHandler, packetHandler, ADDR_PRO_PRESENT_VELOCITY, ADDR_PRO_PRESENT_VELOCITY_LENGTH);
+    dynamixel::GroupSyncRead groupSyncReadProfileVelocity(portHandler, packetHandler, ADDR_PRO_PROFILE_VELOCITY, ADDR_PRO_PROFILE_VELOCITY_LENGTH);
+    dynamixel::GroupSyncRead groupSyncReadProfileAcceleration(portHandler, packetHandler, ADDR_PRO_PROFILE_ACCELERATION, ADDR_PRO_PROFILE_ACCELERATION_LENGTH);
+    dynamixel::GroupSyncRead groupSyncReadCurrent(portHandler, packetHandler, ADDR_PRO_PRESENT_CURRENT, ADDR_PRO_PRESENT_CURRENT_LENGTH);
+    if (portHandler->openPort())
+    {
+        printf("Succeeded to open the port!\n");
+    }
+    if (portHandler->setBaudRate(BAUDRATE))
+    {
+        printf("Succeeded to change the baudrate!\n");
+    }
 }
--- vary_exp/AMC_holding_oneleg.py
+++ vary_exp/AMC_holding_oneleg.py
@@ -11,25 +11,17 @@ p_p = 0.0  # present position
 beta = 0.01  # parameter
 a = 20.0  # parameter
 b = 5.0  # parameter
-motor0 = DxlAPI([13], 'COM3')
+motor0 = DxlAPI([2], '/dev/tty.usbserial-FT2XP85O')
 motor0.set_operating_mode('t')
 motor0.torque_enable()
 print('begin!')
+K = 0.5
+D = 0.5
 for i in range(2000):
-    A = time.time()
     v_p = motor0.get_velocity()
     p_p = motor0.get_position()
-    v_e = v_p - v_t  # velocity error
-    p_e = p_p - p_t  # position error
-    tra_diff = p_e + beta * v_e  # track difference error(t)
-    co_diff = a / (1 + b * np.linalg.norm(tra_diff)**2)  # gamma(t)
-    ff = tra_diff / co_diff  # force F(t)
-    p_gain = ff * p_e
-    d_gain = ff * v_e
-    calc_torque = -ff - p_gain * p_e - d_gain * v_e
+    calc_torque = K*(0 - p_p) + D*(0-v_p)
     motor0.set_torque(calc_torque)
-    B = time.time()
-    print i, calc_torque, B-A
 motor0.torque_disable()
 
 print('ok!')
